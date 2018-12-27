c2ed20f30d54601122826b53264ce2530685608c
Mariana <mvoro@softserveinc.com>
2018-12-27 14:12:33 +0200

    Initial commit

---
+++ src/main/java/com/soap/demo/springbootdemoexample/SpringBootDemoExampleApplication.java
@@ -0,0 +1,14 @@
+package com.soap.demo.springbootdemoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoExampleApplication.class, args);
+	}
+
+}
+
